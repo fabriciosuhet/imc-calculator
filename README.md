49b342a1c761205d090ad8617c7abdc6c1ba68c4
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-08-07 18:46:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple Calculator IMC with setState
---
+++ lib/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_default_state_manager/setState/imc_setstate_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void _goToPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => page,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _goToPage(context, const ImcSetstatePage()),
+              child: const Text('setState'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('ValueNotifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('ChangeNotifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Bloc Pattern (Stream)'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
